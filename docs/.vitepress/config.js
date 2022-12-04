c00b7b01a6c2889f6c068fe3e6da283c7cc33570
Arhell <arhell333@gmail.com>
2022-12-04 04:12:56 +0200

    add favicons

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,17 @@ export default {
   title: 'luya.io',
   description: 'BUILD ANY SYSTEM',
   lang: 'en-US',
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "76x76", href: "/favicons/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicons/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicons/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/favicons/site.webmanifest"}],
+    ['link', { rel: "mask-icon", href: "/favicons/safari-pinned-tab.svg", color: "#42b883"}],
+    ['link', { rel: "shortcut icon", href: "/favicons/favicon.ico"}],
+    ['meta', { name: "msapplication-TileColor", content: "#ffffff"}],
+    ['meta', { name: "msapplication-config", content: "/favicons/browserconfig.xml"}],
+    ['meta', { name: "theme-color", content: "#ffffff"}],
+  ],
   themeConfig: {
     editLink: {
       pattern: 'https://github.com/luyadev/luya.io/edit/master/docs/:path'
